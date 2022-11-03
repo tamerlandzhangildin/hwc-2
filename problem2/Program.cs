72be00b2a8c36f136a12acc87231f0c89c824563
Tamerlan <dzhangtim@list.ru>
2022-11-03 22:33:18 +0300

    newprogramm2

--- problem2/Program.cs
+++ problem2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(Console.Readline()); 
+
+if (n < 100)
+{
+    Console.WriteLine("Error");
+}
+else
+{
+    int coef =  10; 
+
+    if (n > 999)
+    {
+        while (true) 
+        {
+            int sep = n / coef;
+            if (sep > 100 && sep < 999) 
+            {
+                break;
+            }
+            else
+            {
+                coef = *10;
+            }
+        }
+        Console.WriteLine($"The third number is { (n  / coef) % 10 }");
+    }
+else
+ {
+    Console.WriteLine($"The third number is { n % 10 }");
+ }
+
+}
+
+    
