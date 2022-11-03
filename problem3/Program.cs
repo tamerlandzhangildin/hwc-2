4fd0177a22c63d7ac8e5d43ee386dfddef8c39bd
Tamerlan <dzhangtim@list.ru>
2022-11-03 22:46:38 +0300

    newprogramm3

---
+++ problem3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter day number - ");
+string sa = Console.Readline();
+int a = int.Parse(sa);
+
+if (a ==6 && a==7){
+    Console.WriteLine("weekend");
+}
+if (a >= 1 && a  <= 5){
+    Console.WriteLine("working day");
+}
+
